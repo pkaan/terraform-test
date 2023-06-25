a88688444e85ec994d0bf057a4615df1c66942d6
Peetu Kaan <aripeetu.kaan@gmail.com>
2023-06-25 18:28:28 +0100

    added asp

--- main.tf
+++ main.tf
@@ -12,9 +12,25 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "4b9c23f1-02d5-4863-9c82-9fa1098c566d"
+
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "pkaan_terraform_group"
   location = "eastus2"
+  tags = {
+    environment = "sbx"
+    platform    = "Terraform"
+  }
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-pkaan-terraform"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "F1"
 }
